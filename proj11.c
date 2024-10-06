35422efc9d1a6d8935f2c892ae61fb037c9dfb94
CyberDani9 <daniellegonsalves24@gmail.com>
2024-10-06 15:42:04 +0530

    fourth

---
+++ proj11.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int A,B,add,diff,pro,quo;
+    printf("enter no a");
+    scanf("%d",&A);
+    printf("enter no b");
+    scanf("%d",&B);
+    add=A+B;
+    diff=A-B;
+    pro=A*B;
+    quo=A/B;
+    printf("the sum is %d \n the difference is %d \n the product is %d \n and the quotient is %d",add,diff,pro,quo);
+    }
